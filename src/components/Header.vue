c72293394ff7360e61bad6e50965ee1d834ca2b8
MattiaPortanti <mattia.portantipm@gmail.com>
2022-04-07 13:20:44 +0200

    aggiunti due components per l'header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,16 +1,28 @@
 <template>
     <header>
-
+        <HeaderNav/>
+        <HeaderJumbo/>
     </header>
 </template>
 
 <script>
+import HeaderNav from './HeaderComponents/HeaderNav.vue';
+import HeaderJumbo from './HeaderComponents/HeaderJumbo.vue'; 
+
 export default {
-    name: 'IndexHeader'
+    name: 'IndexHeader',
+    components : {
+        HeaderNav,
+        HeaderJumbo
+    }
 
 }
 </script>
 
 <style lang='scss'>
+    
+    header{
+        background-color: black;
+    }
 
 </style>
---
+++ src/components/HeaderComponents/HeaderNav.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="container">
+        <section class="row">
+            <div class="col-12">
+                <button type="button" class="btn btn-danger btn-sm">Danger</button>
+                
+                <div>
+                    
+                </div>
+            </div>
+        </section>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'HeaderNav'
+
+}
+</script>
+
+<style lang='scss' scoped>
+    
+    nav.container{
+        height: 10vh;
+    }
+
+</style>
