fb45b4be31d89bb2ac2de216dd31c780d6f53eae
MattiaPortanti <mattia.portantipm@gmail.com>
2022-04-07 12:38:00 +0200

    impostati documenti di lavoro, cretati components principali e importato bootstrap

---
+++ src/components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <footer>
+
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'IndexFooter'
+
+}
+</script>
+
+<style lang='scss'>
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,16 @@
+<template>
+    <main>
+
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'IndexMain'
+
+}
+</script>
+
+<style lang='scss'>
+
+</style>
