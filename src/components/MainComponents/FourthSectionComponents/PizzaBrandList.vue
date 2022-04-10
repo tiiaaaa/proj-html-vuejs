430792965fc89d087361f8dec6e891f21f9866bd
MattiaPortanti <mattia.portantipm@gmail.com>
2022-04-10 23:15:06 +0200

    Creato Layout e popolato dinamicamente component PizzaBrandList

--- src/components/MainComponents/FourthSectionComponents/PizzaBrandList.vue
+++ src/components/MainComponents/FourthSectionComponents/PizzaBrandList.vue
@@ -1,16 +1,60 @@
 <template>
-    <div>
-
-    </div>
+    <section id="pizza-brand-list" class="row">
+        <div class="col-12 d-flex align-items-center">
+            <ul class="d-flex mb-0">
+                <li v-for="(brand, index) in PizzaBrandList" :key="index">
+                    <a class="text-decoration-none" href="#">
+                        <img :src="require('@/assets/img/' + brand.url)" alt="">
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
 </template>
 
 <script>
 export default {
     name:'PizzaBrandList',
+    data: function(){
+        return{
+
+            PizzaBrandList:[
+
+                    {
+                        url: 'h1-clients-img-1.png'
+                    },
+
+                    {
+                        url: 'h1-clients-img-2.png'
+                    },
+
+                    {
+                        url: 'h1-clients-img-3.png'
+                    },
+
+                    {
+                        url: 'h1-clients-img-4.png'
+                    },
+
+                    {
+                        url: 'h1-clients-img-5.png'
+                    },
+            ]
+        }
+    }
 
 }
 </script>
 
 <style lang='scss' scoped>
+
+    section#pizza-brand-list{
+        height: 40vh;
+        background-color: #f7f7f2;
+
+        li{
+            list-style-type: none;
+        }
+    }
 
 </style>
